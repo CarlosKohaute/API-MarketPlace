ac5d0e9fd272ec07759436dab5d1c8488b4e39e8
Carlos Kohaute <97993704+CarlosKohaute@users.noreply.github.com>
2022-04-25 22:17:18 -0300

    commit

--- src/routes/cell.route.js
+++ src/routes/cell.route.js
@@ -3,19 +3,29 @@ const { Router } = require('express');
 const controllerCells = require('../controllers/cell.controller');
 const { validId, validObjectBody } = require('../middlewares/cell.middleware');
 
-route.get('/find-cells', controllerCells.findAllCellsController);
+route.get(
+  '/find-cells', 
+controllerCells.findAllCellsController
+);
 route.get(
   '/find-one-cell/:id',
   validId,
   controllerCells.findByIdCellController,
 );
-route.post('/create', validObjectBody, controllerCells.createCellController);
+route.post(
+  '/create',
+   validObjectBody, 
+   controllerCells.createCellController
+   );
 route.put(
   '/update/:id',
   validId,
   validObjectBody,
   controllerCells.updateCellController,
 );
-route.delete('/delete/:id', validId, controllerCells.deleteCellController);
+route.delete(
+  '/delete/:id', 
+  validId,
+   controllerCells.deleteCellController);
 
 module.exports = route;
