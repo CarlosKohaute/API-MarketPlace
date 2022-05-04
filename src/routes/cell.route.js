f957e271797e2c5a17f5380172a8935368472da8
Carlos Kohaute <97993704+CarlosKohaute@users.noreply.github.com>
2022-05-04 00:08:57 -0300

    commit swagger

--- src/routes/cell.route.js
+++ src/routes/cell.route.js
@@ -1,13 +1,19 @@
 const route = require('express').Router();
 const { Router } = require('express');
 const controllerCells = require('../controllers/cell.controller');
+const cartController = require('../controllers/cart.controller');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('../../swagger.json');
+
+route.use('/api-docs', swaggerUi.serve);
+route.get('/api-docs', swaggerUi.setup(swaggerDocument));
 
 const {
   validId,
   validObjectBody,
   validObjectBodyCart,
 } = require('../middlewares/cell.middleware');
-const cartController = require('../controllers/cart.controller');
+
 
 route.get('/find-cells', controllerCells.findAllCellsController);
 route.get(
